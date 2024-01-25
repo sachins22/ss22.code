e07c22fed6410bea2e68810f6426c4fbd9cda788
sachins22 <155816007+sachins22@users.noreply.github.com>
2024-01-25 19:52:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 // im.addEventListener("click",()=>{
 //     im.innerHTML="c:\ss22.code\radha-krishna2.jpg"
 // })
+let img=document.querySelector("#img")
+img.addEventListener("click",()=>{
+    img.src="radha-krishna3.jpg"
+})
